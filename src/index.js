1bdf6ecbc67587daec5188dedffad34d1016bf8e
kolja <koljaz@web.de>
2019-09-23 23:01:40 +0200

    Created example of problem.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import deepdash from 'deepdash';
+
+deepdash(_);
+
+function component() {
+    const element = document.createElement('div');
+
+    // Lodash, currently included via a script, is required for this line to work
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    return element;
+}
+
+document.body.appendChild(component());
